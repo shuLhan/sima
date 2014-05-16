5b9bc1061db454850e781658a36efebfbd4da2f0
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-17 05:54:09 +0700

    module/init: use Jaring::checkCookies.

--- module/init.php
+++ module/init.php
@@ -6,11 +6,4 @@ function __autoload($class_name) {
 }
 
 Jaring::init ();
-
-$m_home	= Jaring::$_path . Jaring::$_path_mod ."/home/";
-
-if (Jaring::$_c_uid === 0 && strpos ($_SERVER['REQUEST_URI'], $m_home)) {
-	header ("Location:". $m_home);
-	die ();
-}
-?>
+Jaring::checkCookies ();
