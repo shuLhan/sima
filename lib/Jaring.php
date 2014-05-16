80045e21b75d2b2b4f02f1569ec8c0c17531f7b3
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-17 05:53:12 +0700

    Jaring: add function checkAccess and handleRequest.
    * _out: variable to save json output.
    * checkAccess: server side check of user permission.
    * handleRequest: simple RESTful.

--- lib/Jaring.php
+++ lib/Jaring.php
@@ -25,7 +25,15 @@ class Jaring
 	public static $MSG_SUCCESS_UPDATE	= 'Data has been updated.';
 	public static $MSG_SUCCESS_CREATE	= 'New data has been created.';
 	public static $MSG_SUCCESS_DESTROY	= 'Data has been deleted.';
+	public static $MSG_ACCESS_FAIL		= "You don't have sufficient privilege.";
+	public static $MSG_REQUEST_INVALID	= "Invalid request ";
 	public static $MOD_INIT				= '/init';
+
+	public static $ACCESS_NO		= 0;
+	public static $ACCESS_READ		= 1;
+	public static $ACCESS_CREATE	= 2;
+	public static $ACCESS_UPDATE	= 3;
+	public static $ACCESS_DELETE	= 4;
 
 	public static $_ext				= '.php';
 	public static $_title			= 'Jaring Framework';
@@ -44,12 +52,53 @@ class Jaring
 	public static $_db_pool_max		= 100;
 	public static $_db				= null;
 
+	public static $_out				= array (
+										'success'	=> false
+									,	'data'		=> ''
+									,	'total'		=> 0
+									);
+
 	/*
 		Cookies values.
 		Variables that will be instantiated when calling getCookiesValue.
 	*/
 	public static $_c_uid			= 0;
 	public static $_c_username		= 'Anonymous';
+
+	public static function checkAccess ($mod, $uid, $access)
+	{
+		$q	="
+				select	GM.permission
+				from	_user			U
+				,		_group			G
+				,		_user_group		UG
+				,		_menu			M
+				,		_group_menu		GM
+				where	GM._group_id	= G.id
+				and		GM._menu_id		= M.id
+				and		UG._group_id	= G.id
+				and		UG._user_id		= U.id
+				and		M.module		= '". $mod ."'
+				and		U.id			= ". $uid ."
+				order by GM.permission desc
+				limit	0,1
+			";
+
+		try {
+			$ps = Jaring::$_db->prepare($q);
+			$ps->execute ();
+			$rs = $ps->fetchAll (PDO::FETCH_ASSOC);
+			$ps->closeCursor ();
+
+			if (((int) $rs[0]["permission"]) >= $access) {
+				return true;
+			}
+		} catch (Exception $e) {
+			return false;
+		}
+
+		return false;
+	}
 
 	public static function initDB ()
 	{
@@ -113,6 +162,16 @@ class Jaring
 		}
 	}
 
+	public static function checkCookies ()
+	{
+		$m_home	= Jaring::$_path . Jaring::$_path_mod ."/home/";
+
+		if (Jaring::$_c_uid === 0 && strpos ($_SERVER['REQUEST_URI'], $m_home)) {
+			header ("Location:". $m_home);
+			die ();
+		}
+	}
+
 	public static function init ()
 	{
 		$f_app_conf	= APP_PATH ."/app.conf";
@@ -139,5 +198,79 @@ class Jaring
 		self::$_db_pool_max		= $app_conf['db.pool.max'];
 
 		Jaring::getCookiesValue ();
+	}
+
+	public static function handleRequest ()
+	{
+		$i		= 1;
+		$q		= "";
+		$t		= 0;
+
+		$method	= $_SERVER["REQUEST_METHOD"];
+		$uri	= explode ("?", $_SERVER["REQUEST_URI"])[0];
+		$path	= APP_PATH.$uri;
+		$access	= Jaring::$ACCESS_NO;
+		$module	= trim (
+					str_replace (
+						"/"
+					,	"_"
+					,	substr (
+							strstr (
+								$uri
+							,	self::$_path_mod
+							)
+						,	strlen(self::$_path_mod)
+						)
+					)
+				,	"_"
+				);
+
+		try {
+			Jaring::initDB ();
+
+			switch ($method) {
+			case "GET":
+				$access	= Jaring::$ACCESS_READ;
+				break;
+			case "POST":
+				$access	= Jaring::$ACCESS_CREATE;
+				break;
+			case "PUT":
+				$access	= Jaring::$ACCESS_UPDATE;
+				break;
+			case "DELETE":
+				$access	= Jaring::$ACCESS_DELETE;
+				break;
+			default:
+				Jaring::$_out["data"]	= Jaring::$MSG_REQUEST_INVALID
+										. $_SERVER["REQUEST_METHOD"];
+				break;
+			}
+
+			$s = Jaring::checkAccess ($module, Jaring::$_c_uid, $access);
+
+			if (false === $s) {
+				throw new Exception (Jaring::$MSG_ACCESS_FAIL);
+			}
+
+			switch ($access) {
+			case 1:
+				require_once $path."read.php";
+				break;
+			case 2:
+				require_once $path."create.php";
+				break;
+			case 3:
+				require_once $path."update.php";
+				break;
+			case 4:
+				require_once $path."delete.php";
+				break;
+			}
+		} catch (Exception $e) {
+			Jaring::$_out['data'] = $e->getMessage ();
+		}
+
+		echo json_encode (Jaring::$_out);
 	}
 }
