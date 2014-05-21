4cbfcaf6683323491e135134853a303897568fa7
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-22 06:50:47 +0700

    [sima] Reference.Asset.Procurement: use internal CRUD handler.

--- module/Reference/Asset/Procurement/index.php
+++ module/Reference/Asset/Procurement/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once "../../../init.php";
 
-Jaring::$_mod["db_table"] = "asset_procurement";
+Jaring::$_mod["db_table"]["name"]	= "asset_procurement";
+Jaring::$_mod["db_table"]["id"]		= ["id"];
+Jaring::$_mod["db_table"]["read"]	= ["id", "name"];
+Jaring::$_mod["db_table"]["search"]	= ["name"];
+Jaring::$_mod["db_table"]["create"]	= ["name"];
+Jaring::$_mod["db_table"]["update"]	= ["name"];
+Jaring::$_mod["db_table"]["order"]	= ["name"];
 
 Jaring::handleRequest ();
