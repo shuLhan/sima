b85c3f4acc98a2282fb1f90dff985baff9664235
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-22 07:47:38 +0700

    [jaring] System.Menu: create, read, and update using internal handler.

--- module/System/Menu/index.php
+++ module/System/Menu/index.php
@@ -1,3 +1,24 @@
 <?php
 require_once "../../init.php";
+
+$fields = [
+			"id"
+		,	"pid"
+		,	"type"
+		,	"label"
+		,	"icon"
+		,	"image"
+		,	"module"
+		,	"description"
+		];
+
+Jaring::$_mod["db_table"]["name"]	= "_menu";
+Jaring::$_mod["db_table"]["id"]		= ["id"];
+Jaring::$_mod["db_table"]["read"]	= $fields;
+Jaring::$_mod["db_table"]["search"]	= ["label", "module", "description"];
+Jaring::$_mod["db_table"]["order"]	= ["id", "pid"];
+Jaring::$_mod["db_table"]["create"]	= $fields;
+Jaring::$_mod["db_table"]["update"]	= $fields;
+
+
 Jaring::handleRequest ();
