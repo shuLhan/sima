521b75c6e9e3361e9333953f34669472680c4db7
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-22 07:27:54 +0700

    [jaring] System.Group: Create/update using internal handler.

--- module/System/Group/index.php
+++ module/System/Group/index.php
@@ -1,3 +1,9 @@
 <?php
 require_once "../../init.php";
+
+Jaring::$_mod["db_table"]["name"]	= "_group";
+Jaring::$_mod["db_table"]["id"]		= ["id"];
+Jaring::$_mod["db_table"]["create"]	= ["pid", "name"];
+Jaring::$_mod["db_table"]["update"]	= ["pid", "name"];
+
 Jaring::handleRequest ();
