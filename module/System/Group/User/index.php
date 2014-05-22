2ca613ad9016afd11848861d791828483488d794
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-22 07:51:57 +0700

    [jaring] System.Group.User: create, delete, update using internal handler.

--- module/System/Group/User/index.php
+++ module/System/Group/User/index.php
@@ -1,3 +1,9 @@
 <?php
 require_once "../../../init.php";
+
+Jaring::$_mod["db_table"]["name"]	= "_user_group";
+Jaring::$_mod["db_table"]["id"]		= ["id"];
+Jaring::$_mod["db_table"]["create"]	= ["_group_id", "_user_id"];
+Jaring::$_mod["db_table"]["update"]	= ["_user_id"];
+
 Jaring::handleRequest ();
