d3c46e195ba8cf23f77c0221bce8d1840ac4bcb2
Mhd Sulhan <m.shulhan@gmail.com>
2014-05-22 07:28:37 +0700

    [jaring] System.User: Read and delete using internal handler.

--- module/System/User/index.php
+++ module/System/User/index.php
@@ -1,3 +1,16 @@
 <?php
 require_once "../../init.php";
+
+Jaring::$_mod["db_table"]["name"]	= "_user";
+Jaring::$_mod["db_table"]["id"]		= ["id"];
+Jaring::$_mod["db_table"]["read"]	= [
+										"id"
+									,	"name"
+									,	"realname"
+									,	"password as password_old"
+									,	"'' as password"
+									];
+Jaring::$_mod["db_table"]["search"]	= ["name", "realname"];
+Jaring::$_mod["db_table"]["order"]	= ["name"];
+
 Jaring::handleRequest ();
