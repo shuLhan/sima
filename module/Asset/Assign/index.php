473c87c3274f6ad29c4213c1f37b694e843983c9
Mhd Sulhan <m.shulhan@gmail.com>
2014-06-13 20:04:39 +0700

    [sima] Add module asset assignment.

---
+++ module/Asset/Assign/index.php
@@ -0,0 +1,33 @@
+<?php
+/*
+	Copyright 2014 - Mhd Sulhan
+	Authors:
+		- mhd.sulhan (m.shulhan@gmail.com)
+*/
+
+require_once "../../init.php";
+
+$fields	=
+[
+	"id"
+,	"asset_id"
+,	"cost"
+,	"assign_date"
+,	"_user_id"
+,	"location_id"
+,	"location_detail"
+,	"description"
+];
+
+Jaring::$_mod["db_table"]["name"]	= "asset_assign_log";
+Jaring::$_mod["db_table"]["id"]		= ["id"];
+Jaring::$_mod["db_table"]["read"]	= $fields;
+Jaring::$_mod["db_table"]["search"]	= [
+										"location_detail"
+									,	"description"
+									];
+Jaring::$_mod["db_table"]["create"]	= array_slice ($fields, 1);
+Jaring::$_mod["db_table"]["update"]	= array_slice ($fields, 1);
+Jaring::$_mod["db_table"]["order"]	= ["assign_date DESC"];
+
+Jaring::handleRequest ("crud");
---
+++ module/Asset/Assign/layout.js
@@ -0,0 +1,213 @@
+/*
+	Copyright 2014 - Mhd Sulhan
+	Authors:
+		- mhd.sulhan (m.shulhan@gmail.com)
+*/
+
+function JxAssetAssign ()
+{
+	this.id		= "Asset_Assign";
+	this.dir	= Jx.generateModDir (this.id);
+
+//{{{ stores
+	this.storeSystemUser	= Ext.create ("Jx.StoreRest", {
+			url				:Jx.generateModDir ("System_User")
+		,	fields				:
+			[{
+				name	:"id"
+			,	type	:"int"
+			},{
+				name	:"realname"
+			}]
+		});
+//}}}
+//{{{ fields
+	this.fields		= [{
+			header		:"ID"
+		,	dataIndex	:"id"
+		,	hidden		:true
+		,	type		:"int"
+		,	editor		:
+			{
+				hidden	:true
+			}
+		},{
+			header		:"Asset ID"
+		,	dataIndex	:"asset_id"
+		,	hidden		:true
+		,	type		:"int"
+		,	editor		:
+			{
+				hidden		:true
+			}
+		},{
+			header		:"Date"
+		,	xtype		:"datecolumn"
+		,	dataIndex	:"assign_date"
+		,	format		:"d M Y"
+		,	editor		:
+			{
+				xtype		:"datefield"
+			,	format		:"d M Y"
+			}
+		},{
+			header		:"Cost"
+		,	dataIndex	:"cost"
+		,	type		:"float"
+		,	editor		:
+			{
+				xtype		:"numberfield"
+			,	minValue	:0
+			}
+		},{
+			header		:"User"
+		,	dataIndex	:"_user_id"
+		,	renderer	:this.storeSystemUser.renderData ("id", "realname")
+		,	editor		:
+			{
+				xtype			:"combobox"
+			,	store			:this.storeSystemUser
+			,	valueField		:"id"
+			,	displayField	:"realname"
+			}
+		},{
+			header		:"Location"
+		,	dataIndex	:"location_id"
+		,	renderer	:Jx.app.store.Asset.Location.renderData ("id", "name")
+		,	editor		:
+			{
+				xtype			:"combobox"
+			,	store			:Jx.app.store.Asset.Location
+			,	valueField		:"id"
+			,	displayField	:"name"
+			}
+		},{
+			header		:"Detail Location"
+		,	dataIndex	:"location_detail"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textarea"
+			}
+		},{
+			header		:"Description"
+		,	dataIndex	:"description"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textarea"
+			}
+		}];
+//}}}
+
+//{{{ panel: asset
+	this.panelAsset			= Ext.create ("Jx.GridPaging", {
+			store			:Jx.app.store.Asset
+		,	title			:"Aset"
+		,	region			:"center"
+		,	showCrudButtons	:false
+		,	columns			:
+			[{
+				header			:"Barcode"
+			,	dataIndex		:"barcode"
+			,	width			:130
+			},{
+				header			:"Type"
+			,	dataIndex		:"type_id"
+			,	renderer		:Jx.app.store.Asset.Type.renderData ("id", "name")
+			,	flex			:true
+			},{
+				header			:"Merk"
+			,	dataIndex		:"merk"
+			,	flex			:true
+			},{
+				header			:"Model"
+			,	dataIndex		:"model"
+			,	flex			:true
+			}]
+
+		,	onSelectionChange : function (model, data)
+			{
+				var id = 0;
+
+				if (data.length > 0) {
+					id = data[0].get ("id");
+				}
+
+				Asset_Assign.panelAssetAssignLog.doReload (this.perm, id);
+			}
+		});
+//}}}
+//{{{ panel: assign log
+	this.panelAssetAssignLog = Ext.create ("Jx.CardGridForm", {
+			title			:"Riwayat Relokasi"
+		,	url				:this.dir
+		,	region			:"east"
+		,	width			:"50%"
+		,	split			:true
+		,	collapsible		:true
+		,	fields			:this.fields
+		,	gridConfig		:
+			{
+				showCrudButtons	:true
+			,	showPagingBar	:false
+			}
+
+		,	doReload	: function (perm, id)
+			{
+				if (id <= 0) {
+					this.store.removeAll ();
+				} else {
+					this.store.proxy.extraParams.asset_id = id;
+					this.doRefresh (perm);
+				}
+			}
+
+		,	afterAdd : function ()
+			{
+				this.form.getForm ().setValues ({
+					"asset_id" : this.store.proxy.extraParams.asset_id
+				});
+			}
+		});
+//}}}
+//{{{ panel: main
+	this.panel			= Ext.create ("Ext.container.Container", {
+			layout		:"border"
+		,	title		:"Relokasi Aset"
+		,	closable	:true
+		,	items		:
+			[
+				this.panelAsset
+			,	this.panelAssetAssignLog
+			]
+		});
+//}}}
+//{{{ refresh this module
+	this.doRefresh	= function (perm)
+	{
+		var self = this;
+
+		Jx.chainStoreLoad (
+				[
+					this.storeSystemUser
+				,	Jx.app.store.Asset.Type
+				,	Jx.app.store.Asset.Procurement
+				,	Jx.app.store.Asset.Status
+				,	Jx.app.store.Asset.Location
+				,	Jx.app.store.Asset
+				]
+			,	function ()
+				{
+					self.perm = perm;
+					self.panelAsset.doRefresh (perm);
+					self.panelAssetAssignLog.doReload (perm, 0);
+				}
+			,	0);
+	};
+//}}}
+}
+
+var Asset_Assign = new JxAssetAssign ();
+
+//# sourceURL=module/Asset/Assign/layout.js
