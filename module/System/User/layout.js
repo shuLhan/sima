3e9d0c92e11f2ed027c5126e2745544bf2e55d82
Mhd Sulhan <m.shulhan@gmail.com>
2014-06-19 12:07:54 +0700

    [jaring] System.User: create panel by extending Jx.GridPaging.FormEditor.

--- module/System/User/layout.js
+++ module/System/User/layout.js
@@ -4,83 +4,87 @@
 		- mhd.sulhan (m.shulhan@gmail.com)
 */
 
-function JxSystemUser ()
-{
-	this.id		= "System_User";
-	this.dir	= Jx.generateModDir (this.id);
-
-	this.store	= Ext.create ("Jx.StoreRest", {
-		url		:this.dir
-	,	fields	:
-		[
-			"id"
-		,	"name"
-		,	"realname"
-		,	"password"
-		,	"password_old"
-		]
-	});
-
-	this.panel			= Ext.create ("Jx.GridPaging.FormEditor", {
-			itemId		:this.id
-		,	store		:this.store
-		,	panelConfig	:
-			{
-				title		:"System User"
-			,	closable	:true
-			}
-		,	columns		:
-			[{
-				header		:"ID"
-			,	dataIndex	:"id"
-			,	hidden		:true
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	hidden		:true
-				}
-			},{
-				header		:"User ID"
-			,	dataIndex	:"name"
-			,	flex		:1
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	vtype		:"alphanum"
-				,	allowBlank	:false
-				}
-			},{
-				header		:"User name"
-			,	dataIndex	:"realname"
-			,	flex		:1
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	allowBlank	:false
-				}
-			},{
-				header		:"Current Password"
-			,	dataIndex	:"password_old"
-			,	hidden		:true
-			},{
-				header		:"Password"
-			,	dataIndex	:"password"
-			,	hidden		:true
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	vtype		:"alphanum"
-				,	inputType	:"password"
-				}
-			}]
-		});
-
-	this.doRefresh	= function (perm)
+Ext.define ("Jx.app.System.User", {
+	extend	:"Jx.GridPaging.FormEditor"
+,	config	:
 	{
-		this.panel.doRefresh (perm);
-	};
-};
+		itemId		:"System_User"
+	,	panelConfig	:
+		{
+			title		:"System User"
+		,	closable	:true
+		}
+	,	columns		:
+		[{
+			header		:"ID"
+		,	dataIndex	:"id"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	hidden		:true
+			}
+		},{
+			header		:"User ID"
+		,	dataIndex	:"name"
+		,	flex		:1
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	vtype		:"alphanum"
+			,	allowBlank	:false
+			}
+		},{
+			header		:"User name"
+		,	dataIndex	:"realname"
+		,	flex		:1
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	allowBlank	:false
+			}
+		},{
+			header		:"Current Password"
+		,	dataIndex	:"password_old"
+		,	hidden		:true
+		},{
+			header		:"Password"
+		,	dataIndex	:"password"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	vtype		:"alphanum"
+			,	inputType	:"password"
+			}
+		}]
+	}
 
-var System_User = new JxSystemUser ();
+,	constructor : function (config)
+	{
+		var opts	= {};
+		var dir		= Jx.generateModDir (this.config.itemId);
+		var store	= Ext.create ("Jx.StoreRest", {
+				url		:dir
+			,	fields	:
+				[
+					"id"
+				,	"name"
+				,	"realname"
+				,	"password"
+				,	"password_old"
+				]
+			});
+
+		Ext.merge (opts, {
+				store	: store
+			});
+		Ext.merge (opts, this.config);
+
+		this.callParent ([opts]);
+	}
+});
+
+var System_User = Ext.create ("Jx.app.System.User");
 
 //# sourceURL=module/System/User/layout.js
