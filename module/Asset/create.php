49e2706830cbba5559c9f4ec3d2fa21d9d27c8ea
Mhd Sulhan <m.shulhan@gmail.com>
2014-06-28 14:43:39 +0700

    [sima] Asset: assign current user when creating new asset.

--- module/Asset/create.php
+++ module/Asset/create.php
@@ -13,3 +13,34 @@ foreach ($data as &$v) {
 //{{{ save it to database
 Jaring::handleRequestCreate ($data);
 //}}}
+
+//{{{ assign current user as the owner
+$uid = Jaring::$_c_uid;
+$q = "
+	insert into asset_assign_log (
+		id
+	,	asset_id
+	,	_user_id
+	,	assign_date
+	) values (
+		?
+	,	?
+	,	$uid
+	,	NOW()
+	)
+";
+
+Jaring::$_db_ps = Jaring::$_db->prepare ($q);
+
+foreach ($data as $v) {
+	$bindv = [];
+
+	$bindv[0] = Jaring::generate_id ();
+	$bindv[1] = $v["id"];
+
+	Jaring::$_db_ps->execute ($bindv);
+	Jaring::$_db_ps->closeCursor ();
+
+	unset ($bindv);
+}
+//}}}
