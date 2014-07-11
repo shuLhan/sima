34533ca0ce89c27ca26ad7b2840d6b8fd432c1d5
Mhd Sulhan <m.shulhan@gmail.com>
2014-07-11 16:59:31 +0700

    [jaring] System.Media: refactoring using Ext.define.

--- module/System/Media/layout.js
+++ module/System/Media/layout.js
@@ -3,116 +3,104 @@
 	Authors:
 		- mhd.sulhan (m.shulhan@gmail.com)
 */
-function JxSystemMedia ()
-{
-	this.id		= "System_Media";
-	this.dir	= Jx.generateModDir (this.id);
-
-	this.store		= Ext.create ("Jx.Store", {
-		url			:this.dir
-	,	fields		:
-		[
-			"id"
-		,	"name"
-		,	"extension"
-		,	"size"
-		,	"mime"
-		,	"description"
-		,	"path"
-		]
-	});
-
-	this.panel			= Ext.create ("Jx.GridPaging.FormEditor", {
-			itemId		:this.id
-		,	store		:this.store
-		,	panelConfig	:
-			{
-				title		:"System Media"
-			,	closable	:true
-			}
-		,	formConfig	:
-			{
-				syncUseStore	:false
-			}
-		,	columns		:
-			[{
-				header		:"ID"
-			,	dataIndex	:"id"
-			,	hidden		:true
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	hidden		:true
-				}
-			},{
-				header		:"Name"
-			,	dataIndex	:"name"
-			,	editor		:
-				{
-					xtype		:"textfield"
-				}
-			},{
-				header		:"Extension"
-			,	dataIndex	:"extension"
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	hidden		:true
-				}
-			},{
-				header		:"Size"
-			,	dataIndex	:"size"
-			,	hidden		:true
-			,	editor		:
-				{
-					xtype		:"numberfield"
-				,	hidden		:true
-				}
-			},{
-				header		:"Mime"
-			,	dataIndex	:"mime"
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	hidden		:true
-				}
-			},{
-				header		:"Description"
-			,	dataIndex	:"description"
-			,	flex		:1
-			,	editor		:
-				{
-					xtype		:"textarea"
-				}
-			},{
-				header		:"Path"
-			,	dataIndex	:"path"
-			,	hidden		:true
-			,	editor		:
-				{
-					xtype		:"textfield"
-				,	hidden		:true
-				}
-			},{
-				header		:"Content"
-			,	dataIndex	:"content"
-			,	hidden		:true
-			,	editor		:
-				{
-					xtype		:"filefield"
-				,	itemId		:"content"
-				,	buttonText	:"Select File"
-				,	editable	:false
-				}
-			}]
-		});
-
-	this.doRefresh	= function (perm)
+Ext.define ("Jx.app.System.Media", {
+	extend		:"Jx.GridPaging.FormEditor"
+,	config		:
 	{
-		this.panel.doRefresh (perm);
-	};
-};
+		itemId		:"System_Media"
+	,	title		:"System Media"
+	,	store		:Ext.create ("Jx.StoreRest",
+		{
+			url			:Jx.generateModDir ("System_Media")
+		,	fields		:
+			[
+				"id"
+			,	"name"
+			,	"extension"
+			,	"size"
+			,	"mime"
+			,	"description"
+			,	"path"
+			]
+		})
+	,	formConfig	:
+		{
+			syncUseStore	:false
+		}
+	,	columns		:
+		[{
+			header		:"ID"
+		,	dataIndex	:"id"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	hidden		:true
+			}
+		},{
+			header		:"Name"
+		,	dataIndex	:"name"
+		,	editor		:
+			{
+				xtype		:"textfield"
+			}
+		},{
+			header		:"Extension"
+		,	dataIndex	:"extension"
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	hidden		:true
+			}
+		},{
+			header		:"Size"
+		,	dataIndex	:"size"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"numberfield"
+			,	hidden		:true
+			}
+		},{
+			header		:"Mime"
+		,	dataIndex	:"mime"
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	hidden		:true
+			}
+		},{
+			header		:"Description"
+		,	dataIndex	:"description"
+		,	flex		:1
+		,	editor		:
+			{
+				xtype		:"textarea"
+			}
+		},{
+			header		:"Path"
+		,	dataIndex	:"path"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"textfield"
+			,	hidden		:true
+			}
+		},{
+			header		:"Content"
+		,	dataIndex	:"content"
+		,	hidden		:true
+		,	editor		:
+			{
+				xtype		:"filefield"
+			,	itemId		:"content"
+			,	buttonText	:"Select File"
+			,	editable	:false
+			}
+		}]
+	}
+});
 
-var System_Media = new JxSystemMedia ();
+var System_Media = Ext.create ("Jx.app.System.Media");
 
 //# sourceURL=module/System/Media/layout.js
