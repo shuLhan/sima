f17e20373e63cefd0d75ff2b0c0d0c54b44f6557
Mhd Sulhan <m.shulhan@gmail.com>
2014-09-02 17:01:41 +0700

    [jaring] System.Group.User: replace read.php with internal db_rel option.

--- module/System/Group/User/index.php
+++ module/System/Group/User/index.php
@@ -6,12 +6,42 @@
 */
 require_once "../../../init.php";
 
+$fields =
+	[
+		"_profile_id"
+	,	"id"
+	,	"_user_id"
+	,	"_group_id"
+	];
+
 Jaring::$_mod["db_table"]["name"]	= "_user_group";
-Jaring::$_mod["db_table"]["create"]	= ["_profile_id", "id", "_group_id", "_user_id"];
+Jaring::$_mod["db_table"]["read"]	= $fields;
 Jaring::$_mod["db_table"]["update"]	= ["_user_id"];
+Jaring::$_mod["db_table"]["create"]	= $fields;
+
+Jaring::$_mod["db_rel"]["tables"]		= [ "_user" ];
+Jaring::$_mod["db_rel"]["read"]			=
+	[
+		"_user.name		as _user_name"
+	,	"_user.realname	as _user_realname"
+	];
+
+Jaring::$_mod["db_rel"]["conditions"]	=
+	[
+		"_user_group._user_id"	=> "_user.id"
+	];
+
+Jaring::$_mod["db_rel"]["search"]		=
+	[
+		"_user.name"
+	,	"_user.realname"
+	];
+
+Jaring::$_mod["db_rel"]["order"]		= [ "_user.name" ];
 
 function request_delete_before ($data)
 {
+	// do not delete user admin of profile.
 	foreach ($data as $d) {
 		$ug_id = $d["id"];
 
