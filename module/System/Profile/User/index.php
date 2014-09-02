a9ca4fd34fc5ebfa3f16a8176e3085e780363214
Mhd Sulhan <m.shulhan@gmail.com>
2014-09-02 14:35:55 +0700

    [jaring] System.Profile.User: replace read.php with db_rel options.

--- module/System/Profile/User/index.php
+++ module/System/Profile/User/index.php
@@ -6,14 +6,28 @@
 */
 require_once "../../../init.php";
 
-Jaring::$_mod["db_table"]["name"]		= "_user	U";
+Jaring::$_mod["db_table"]["name"]		= "_user";
 Jaring::$_mod["db_table"]["profiled"]	= false;
 Jaring::$_mod["db_table"]["read"] 		=
 [
-	"U._profile_id"
-,	"U.id"
-,	"U.name"
-,	"U.realname"
+	"_user._profile_id"
+,	"_user.id"
+,	"_user.name"
+,	"_user.realname"
+];
+Jaring::$_mod["db_table"]["order"] 		= ["id"];
+
+Jaring::$_mod["db_rel"]["tables"]		=
+[
+	"_user_group"
+,	"_group"
+];
+
+Jaring::$_mod["db_rel"]["conditions"]	=
+[
+	"_user.id"				=> "_user_group.id"
+,	"_user_group._group_id"	=> "_group.id"
+,	"_group.id"				=> "1"
 ];
 
 Jaring::request_handle ("crud");
